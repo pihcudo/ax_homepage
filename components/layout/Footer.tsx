236f88c976bc6a6adefe7c69be7bb623110f7838
pihcudo <pih1@cudo.co.kr>
2026-04-28 15:24:25 +0900

    footer 변경

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -16,16 +16,13 @@ export default function Footer() {
                 <a href="tel:+82200000000">+82 2 0000 0000</a>
               </li>
               <li style={{ color: 'var(--fg-dim)', fontSize: 12 }}>
-                서울특별시 강남구
-                <br />
-                테헤란로 152
+                과천대로 7길 33(갈현동) DX타워
               </li>
             </ul>
           </div>
         </div>
         <div className="footer__bottom">
           <span>© 2026 CUDO AX 사업부. ALL RIGHTS RESERVED.</span>
-          <span>DESIGN / ENG BY CUDO STUDIO</span>
         </div>
       </div>
     </footer>
